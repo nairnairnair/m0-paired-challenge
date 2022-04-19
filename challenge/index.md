287559c4cdef78355efba9858fa1efa93177a8e1
emmacintosh <emmammrussell@gmail.com>
2022-04-18 20:40:00 -0600

    initial commit

---
+++ challenge/index.md
@@ -0,0 +1,2 @@
+//My Favorite Books //not exactly true but i'm on a timer
+var books = ["ulysses" , "lot49" , "bible" , "demian" , "v"]
---
+++ challenge/newfile.js
@@ -0,0 +1,9 @@
+BOOK FACTS //aw yeah book facts comin at ya, named it this because i had no better ideas
+var bookQuantity = 5
+var booksFlammable = "true" //hopefully not relevant
+var booksLong = 3 //long is relevant
+var bookstore = tatteredCover //i mean, it could be relevant
+var booksInPrint = "true"
+var genre = fiction //4 out of 5 of them, accroding to conventional wisdom
+
+var comics = ["spiderman" , "superman" , "batman" , "aquaman" , "wonderWoman"]
